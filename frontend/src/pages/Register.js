d578a15d8bdc8e588c21d14fc0b9fef514378331
Hemant Singh <115834099+hemantsinghhere@users.noreply.github.com>
2025-01-13 19:17:40 +0530

    Update backend path

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -19,7 +19,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/register", formData);
+      const res = await axios.post("https://captn-assignment.onrender.com/api/auth/register", formData);
       setMessage(res.data.message || "Registration successful!");
 
       if (res.status === 201) {
